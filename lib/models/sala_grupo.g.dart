39c5f22c74304a8b069c4250607b7ad7a14b07e2
Andres Alonso <andresalonso.a@gmail.com>
2023-07-13 12:56:18 -0300

    agregando mnualmente al serializer, no deberia suceder esto

--- lib/models/sala_grupo.g.dart
+++ lib/models/sala_grupo.g.dart
@@ -9,8 +9,10 @@ part of 'sala_grupo.dart';
 SalaGrupo _$SalaGrupoFromJson(Map<String, dynamic> json) => SalaGrupo(
       id: json['id'] as int?,
       sala: json['sala'] as String?,
+      salaId: json['sala_id'] as int?,
       salaColor: Color((json['sala_color'] as int)).withOpacity(1),
       grupo: (json['grupo'] as String),
+      grupoId: json['grupo_id'] as int?,
       horaDesde: DateTime.parse(json['hora_desde']),
       horaHasta: DateTime.parse(json['hora_hasta']),
     );
@@ -18,8 +20,10 @@ SalaGrupo _$SalaGrupoFromJson(Map<String, dynamic> json) => SalaGrupo(
 Map<String, dynamic> _$SalaGrupoToJson(SalaGrupo instance) => <String, dynamic>{
       'id': instance.id,
       'sala': instance.sala,
+      'salaId': instance.salaId,
       'salaColor': instance.salaColor,
       'grupo': instance.grupo,
+      'grupoId': instance.grupoId,
       'horaDesde': instance.horaDesde,
       'horaHasta': instance.horaHasta,
     };
